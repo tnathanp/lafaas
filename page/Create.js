d54a9cd20cfcece17b68113ffc1efec4f5ee2ecb
Tanathan Pansuwan <tanathanp@gmail.com>
2021-02-22 21:49:21 +0700

    Add form validator, Fix some keyboard flickering and bugs

--- page/Create.js
+++ page/Create.js
@@ -1,136 +1,184 @@
 import React, { useState, useEffect } from 'react';
 import { Button, Input } from 'react-native-elements';
-import { StyleSheet, ScrollView, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard, View } from 'react-native';
+import { StyleSheet, ScrollView, KeyboardAvoidingView, View, Keyboard } from 'react-native';
 import { Text } from '../component/Text';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Entypo } from '@expo/vector-icons';
+import validator from 'validator';
 
 const Create = ({ navigation }) => {
-    let lnameInput, usernameInput, emailInput, cemailInput, passInput, cpassInput;
+    let lnameInput, usernameInput, emailInput, passInput, cpassInput;
 
     const [fname, setFname] = useState("");
     const [lname, setLname] = useState("");
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
-    const [emailConfirm, setEmailConfirm] = useState("");
     const [password, setPassword] = useState("");
     const [passwordConfirm, setPasswordConfirm] = useState("");
+    const [formState, setFormState] = useState({
+        fname: 0,
+        lname: 0,
+        username: 0,
+        email: 0,
+        password: 0,
+        cpassword: 0
+    });
+
+    function handleChange(val, field) {
+        let isPass;
+        switch (field) {
+            case 'fname':
+                isPass = validator.isAlpha(val, 'en-US');
+                if (isPass) setFname(val);
+                break;
+            case 'lname':
+                isPass = validator.isAlpha(val, 'en-US');
+                if (isPass) setLname(val);
+                break;
+            case 'username':
+                isPass = validator.isAlphanumeric(val, 'en-US');
+                if (isPass) setUsername(val);
+                break;
+            case 'email':
+                isPass = validator.isEmail(val);
+                if (isPass) setEmail(val);
+                break;
+            case 'password':
+                isPass = validator.isStrongPassword(val, { minSymbols: 0 });
+                if (isPass) setPassword(val);
+                break;
+            case 'cpassword':
+                isPass = password === val && validator.isStrongPassword(val, { minSymbols: 0 });
+                if (isPass) setPasswordConfirm(val);
+                break;
+        }
+
+        if (isPass) {
+            setFormState(prev => {
+                let state = ({ ...prev });
+                state[field] = 0;
+                return state
+            });
+        } else {
+            setFormState(prev => {
+                let state = ({ ...prev });
+                state[field] = 1;
+                return state
+            });
+        }
+    }
 
     return (
         <LinearGradient colors={['#fc8181', '#f6a085']} locations={[0.7, 1]} style={{ flex: 1 }}>
-            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-                <View style={{ flex: 1, padding: 20 }}>
-                    <View style={{ position: 'absolute', top: 50, left: 20 }}>
-                        <Button
-                            title="Back"
-                            icon={<Entypo name="chevron-left" size={16} style={{ marginTop: -3, marginRight: -8 }} color='#fc8181' />}
-                            titleStyle={{ fontSize: 13, fontFamily: 'NotoSans', padding: 10, marginTop: -5, color: '#fc8181' }}
-                            buttonStyle={{ width: 70, height: 26, borderRadius: 20, backgroundColor: 'white' }}
-                            onPress={() => navigation.goBack()}
-                        />
-                    </View>
+            <View style={{ flex: 1, padding: 20 }}>
+                <View style={{ position: 'absolute', top: 50, left: 20 }}>
+                    <Button
+                        title="Back"
+                        icon={<Entypo name="chevron-left" size={16} style={{ marginTop: -3, marginRight: -8 }} color='#fc8181' />}
+                        titleStyle={{ fontSize: 13, fontFamily: 'NotoSans', padding: 10, marginTop: -5, color: '#fc8181' }}
+                        buttonStyle={{ width: 70, height: 26, borderRadius: 20, backgroundColor: 'white' }}
+                        onPress={() => navigation.goBack()}
+                    />
+                </View>
 
-                    <KeyboardAvoidingView style={{ flex: 1, marginTop: 70 }} behavior={Platform.OS == 'ios' ? 'padding' : 'height'}>
-                        <ScrollView showsVerticalScrollIndicator={false}>
-                            <Text style={{ fontSize: 36, fontWeight: 'bold', textAlign: 'center', marginTop: 0, marginBottom: 30 }}>Create Account</Text>
+                <KeyboardAvoidingView style={{ flex: 1, marginTop: 70 }} behavior={Platform.OS == 'ios' ? 'padding' : 'height'}>
+                    <ScrollView showsVerticalScrollIndicator={false}>
+                        <Text style={{ fontSize: 36, fontWeight: 'bold', textAlign: 'center', marginBottom: 30 }}>Create Account</Text>
 
-                            <View style={{ flexDirection: 'row', marginBottom: -15 }}>
-                                <View style={{ width: '50%' }}>
-                                    <Input
-                                        onChangeText={value => setFname({ value })}
-                                        label='First Name'
-                                        style={styles.inputBox}
-                                        labelStyle={styles.label}
-                                        inputStyle={styles.input}
-                                        inputContainerStyle={{ borderBottomColor: 'transparent' }}
-                                        onSubmitEditing={() => lnameInput.focus()}
-                                    />
-                                </View>
-                                <View style={{ width: '50%' }}>
-                                    <Input
-                                        onChangeText={value => setLname({ value })}
-                                        label='Last Name'
-                                        style={styles.inputBox}
-                                        labelStyle={styles.label}
-                                        inputStyle={styles.input}
-                                        inputContainerStyle={{ borderBottomColor: 'transparent' }}
-                                        onSubmitEditing={() => usernameInput.focus()}
-                                        ref={box => { lnameInput = box; }}
-                                    />
-                                </View>
-                            </View>
-
-                            <Input
-                                onChangeText={value => setUsername({ value })}
-                                label='Username'
-                                style={styles.inputBox}
-                                labelStyle={styles.label}
-                                inputStyle={styles.input}
-                                inputContainerStyle={{ borderBottomColor: 'transparent' }}
-                                onSubmitEditing={() => emailInput.focus()}
-                                ref={box => { usernameInput = box; }}
-                            />
-
-                            <Input
-                                onChangeText={value => setEmail({ value })}
-                                label='Email'
-                                style={styles.inputBox}
-                                labelStyle={styles.label}
-                                inputStyle={styles.input}
-                                inputContainerStyle={{ borderBottomColor: 'transparent' }}
-                                onSubmitEditing={() => cemailInput.focus()}
-                                ref={box => { emailInput = box; }}
-                            />
-
-                            <Input
-                                onChangeText={value => setEmailConfirm({ value })}
-                                label='Confirm Email'
-                                style={styles.inputBox}
-                                labelStyle={styles.label}
-                                inputStyle={styles.input}
-                                inputContainerStyle={{ borderBottomColor: 'transparent' }}
-                                containerStyle={{ marginTop: -15 }}
-                                onSubmitEditing={() => passInput.focus()}
-                                ref={box => { cemailInput = box; }}
-                            />
-
-                            <Input
-                                onChangeText={value => setPassword({ value })}
-                                label='Password'
-                                style={styles.inputBox}
-                                labelStyle={styles.label}
-                                inputStyle={styles.input}
-                                inputContainerStyle={{ borderBottomColor: 'transparent' }}
-                                secureTextEntry={true}
-                                onSubmitEditing={() => cpassInput.focus()}
-                                ref={box => { passInput = box; }}
-                            />
-
-                            <Input
-                                onChangeText={value => setPasswordConfirm({ value })}
-                                label='Confirm Password'
-                                style={styles.inputBox}
-                                labelStyle={styles.label}
-                                inputStyle={styles.input}
-                                inputContainerStyle={{ borderBottomColor: 'transparent' }}
-                                containerStyle={{ marginTop: -15 }}
-                                secureTextEntry={true}
-                                onSubmitEditing={() => null}
-                                ref={box => { cpassInput = box; }}
-                            />
-
-                            <View style={{ alignItems: 'center', marginTop: -5 }}>
-                                <Button
-                                    title="create"
-                                    titleStyle={{ padding: 10, marginTop: -3, fontFamily: 'NotoSansBold', color: '#fc8181', fontSize: 14 }}
-                                    buttonStyle={{ width: 300, height: 32, borderRadius: 10, backgroundColor: 'white' }}
-                                    onPress={() => null}
+                        <View style={{ flexDirection: 'row', marginBottom: -15 }}>
+                            <View style={{ width: '50%' }}>
+                                <Input
+                                    onChangeText={value => handleChange(value, 'fname')}
+                                    label='First Name'
+                                    style={formState.fname == 0 ? styles.inputBox : styles.inputBoxError}
+                                    labelStyle={styles.label}
+                                    inputStyle={formState.fname == 0 ? styles.input : styles.inputError}
+                                    inputContainerStyle={{ borderBottomColor: 'transparent' }}
+                                    onSubmitEditing={() => lnameInput.focus()}
+                                    autoCorrect={false}
                                 />
                             </View>
-                        </ScrollView>
-                    </KeyboardAvoidingView>
-                </View>
-            </TouchableWithoutFeedback>
+                            <View style={{ width: '50%' }}>
+                                <Input
+                                    onChangeText={value => handleChange(value, 'lname')}
+                                    label='Last Name'
+                                    style={formState.lname == 0 ? styles.inputBox : styles.inputBoxError}
+                                    labelStyle={styles.label}
+                                    inputStyle={formState.lname == 0 ? styles.input : styles.inputError}
+                                    inputContainerStyle={{ borderBottomColor: 'transparent' }}
+                                    onSubmitEditing={() => usernameInput.focus()}
+                                    ref={box => { lnameInput = box; }}
+                                    autoCorrect={false}
+                                />
+                            </View>
+                        </View>
+
+                        <Input
+                            onChangeText={value => handleChange(value, 'username')}
+                            label='Username'
+                            style={formState.username == 0 ? styles.inputBox : styles.inputBoxError}
+                            labelStyle={styles.label}
+                            inputStyle={formState.username == 0 ? styles.input : styles.inputError}
+                            inputContainerStyle={{ borderBottomColor: 'transparent' }}
+                            onSubmitEditing={() => emailInput.focus()}
+                            ref={box => { usernameInput = box; }}
+                            autoCorrect={false}
+                            autoCapitalize={'none'}
+                        />
+
+                        <Input
+                            onChangeText={value => handleChange(value, 'email')}
+                            label='Email'
+                            style={formState.email == 0 ? styles.inputBox : styles.inputBoxError}
+                            labelStyle={styles.label}
+                            inputStyle={formState.email == 0 ? styles.input : styles.inputError}
+                            inputContainerStyle={{ borderBottomColor: 'transparent' }}
+                            onSubmitEditing={() => passInput.focus()}
+                            ref={box => { emailInput = box; }}
+                            autoCorrect={false}
+                            autoCapitalize={'none'}
+                        />
+
+                        <Input
+                            onChangeText={value => handleChange(value, 'password')}
+                            label='Password'
+                            style={formState.password == 0 ? styles.inputBox : styles.inputBoxError}
+                            labelStyle={styles.label}
+                            inputStyle={formState.password == 0 ? styles.input : styles.inputError}
+                            inputContainerStyle={{ borderBottomColor: 'transparent' }}
+                            onSubmitEditing={() => cpassInput.focus()}
+                            ref={box => { passInput = box; }}
+                            autoCorrect={false}
+                            secureTextEntry={true}
+                            autoCapitalize={'none'}
+                        />
+
+                        <Input
+                            onChangeText={value => handleChange(value, 'cpassword')}
+                            label='Confirm Password'
+                            style={formState.cpassword == 0 ? styles.inputBox : styles.inputBoxError}
+                            labelStyle={styles.label}
+                            inputStyle={formState.cpassword == 0 ? styles.input : styles.inputError}
+                            inputContainerStyle={{ borderBottomColor: 'transparent' }}
+                            containerStyle={{ marginTop: -20, marginBottom: 10 }}
+                            onSubmitEditing={() => Keyboard.dismiss}
+                            ref={box => { cpassInput = box; }}
+                            autoCorrect={false}
+                            secureTextEntry={true}
+                            autoCapitalize={'none'}
+                        />
+
+                        <View style={{ alignItems: 'center', marginTop: -5, marginBottom: 15 }}>
+                            <Button
+                                title="create"
+                                titleStyle={{ padding: 10, marginTop: -3, fontFamily: 'NotoSansBold', color: '#fc8181', fontSize: 14 }}
+                                buttonStyle={{ width: 300, height: 32, borderRadius: 10, backgroundColor: 'white' }}
+                                onPress={() => null}
+                            />
+                        </View>
+                    </ScrollView>
+                </KeyboardAvoidingView>
+            </View>
         </LinearGradient>
     );
 }
@@ -138,14 +186,42 @@ const Create = ({ navigation }) => {
 const styles = StyleSheet.create({
     inputBox: {
         backgroundColor: 'white',
-        opacity: 0.75,
         borderRadius: 10,
-        borderBottomColor: 'transparent'
+        shadowColor: 'black',
+        borderWidth: 1.5,
+        borderColor: 'white',
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.2,
+        shadowRadius: 1.4,
+        elevation: 5
+    },
+    inputBoxError: {
+        backgroundColor: 'white',
+        borderRadius: 10,
+        shadowColor: 'black',
+        borderWidth: 1.5,
+        borderColor: '#FC4E29',
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.2,
+        shadowRadius: 1.4,
+        elevation: 5
     },
     input: {
         fontFamily: 'NotoSansBold',
-        fontSize: 16,
+        fontSize: 15,
         padding: 10
+    },
+    inputError: {
+        fontFamily: 'NotoSansBold',
+        fontSize: 15,
+        padding: 10,
+        color: '#FC4E29'
     },
     label: {
         color: 'white',
--- page/Recover.js
+++ page/Recover.js
@@ -1,72 +1,81 @@
 import React, { useState, useEffect } from 'react';
 import { Button, Input } from 'react-native-elements';
-import { StyleSheet, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard, View, Dimensions } from 'react-native';
+import { StyleSheet, KeyboardAvoidingView, View, Dimensions } from 'react-native';
 import { Text } from '../component/Text';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Entypo } from '@expo/vector-icons';
 import LottieView from 'lottie-react-native';
-
-const deviceWidth = Dimensions.get('window').width;
+import validator from 'validator';
 
 const Recover = ({ navigation }) => {
     let animation;
     useEffect(() => animation.play(), []);
 
     const [email, setEmail] = useState("");
+    const [formState, setFormState] = useState({ email: 0 });
+
+    function handleChange(val) {
+        if (validator.isEmail(val)) {
+            setEmail(val);
+            setFormState({ email: 0 });
+        } else {
+            setFormState({ email: 1 });
+        }
+    }
 
     return (
         <LinearGradient colors={['#fc8181', '#f6a085']} locations={[0.7, 1]} style={{ flex: 1 }}>
-            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-                <View style={{ flex: 1, padding: 20 }}>
-                    <View style={{ position: 'absolute', top: 50, left: 20 }}>
-                        <Button
-                            title="Back"
-                            icon={<Entypo name="chevron-left" size={16} style={{ marginTop: -3, marginRight: -8 }} color='#fc8181' />}
-                            titleStyle={{ fontSize: 13, fontFamily: 'NotoSans', padding: 10, marginTop: -5, color: '#fc8181' }}
-                            buttonStyle={{ width: 70, height: 26, borderRadius: 20, backgroundColor: 'white' }}
-                            onPress={() => navigation.goBack()}
-                        />
-                    </View>
-
-                    <KeyboardAvoidingView style={{ flex: 1, marginTop: 70 }} behavior={Platform.OS == 'ios' ? 'padding' : 'height'}>
-                        <Text style={{ fontSize: 30, fontWeight: 'bold', textAlign: 'center', marginTop: 30, marginBottom: 30 }}>Recover your account</Text>
-                        <View style={{ marginTop: 5 }}>
-                            <Input
-                                onChangeText={value => setEmail({ value })}
-                                label='Enter your email address'
-                                style={styles.inputBox}
-                                labelStyle={styles.label}
-                                inputStyle={styles.input}
-                                inputContainerStyle={{ borderBottomColor: 'transparent' }}
-                                onSubmitEditing={() => null}
-
-                            />
-
-                            <View style={{ alignItems: 'center', marginTop: -5 }}>
-                                <Button
-                                    title="send email"
-                                    titleStyle={{ padding: 10, marginTop: -3, fontFamily: 'NotoSansBold', color: '#fc8181', fontSize: 14 }}
-                                    buttonStyle={{ width: 300, height: 32, borderRadius: 10, backgroundColor: 'white' }}
-                                    onPress={() => null}
-                                />
-                            </View>
-                        </View>
-                    </KeyboardAvoidingView>
-
-                    <LottieView
-                        ref={anim => { animation = anim; }}
-                        style={{
-                            width: 0.4 * Dimensions.get('window').width,
-                            height: 0.4 * Dimensions.get('window').width,
-                            backgroundColor: 'transparent',
-                            alignSelf: 'center',
-                            marginBottom: '60%'
-                        }}
-                        source={require('../assets/anim/8282-search-icon.json')}
+            <View style={{ flex: 1, padding: 20 }}>
+                <View style={{ position: 'absolute', top: 50, left: 20 }}>
+                    <Button
+                        title="Back"
+                        icon={<Entypo name="chevron-left" size={16} style={{ marginTop: -3, marginRight: -8 }} color='#fc8181' />}
+                        titleStyle={{ fontSize: 13, fontFamily: 'NotoSans', padding: 10, marginTop: -5, color: '#fc8181' }}
+                        buttonStyle={{ width: 70, height: 26, borderRadius: 20, backgroundColor: 'white' }}
+                        onPress={() => navigation.goBack()}
                     />
-
                 </View>
-            </TouchableWithoutFeedback>
+
+                <KeyboardAvoidingView style={{ flex: 1, marginTop: 70 }} behavior={Platform.OS == 'ios' ? 'padding' : 'height'}>
+                    <Text style={{ fontSize: 30, fontWeight: 'bold', textAlign: 'center', marginTop: 30, marginBottom: 30 }}>Recover your account</Text>
+                    <View style={{ marginTop: 5 }}>
+
+                        <Input
+                            onChangeText={value => handleChange(value)}
+                            label='Enter your email address'
+                            style={formState.email == 0 ? styles.inputBox : styles.inputBoxError}
+                            labelStyle={styles.label}
+                            inputStyle={formState.email == 0 ? styles.input : styles.inputError}
+                            inputContainerStyle={{ borderBottomColor: 'transparent' }}
+                            onSubmitEditing={() => null}
+                            autoCorrect={false}
+                            autoCapitalize={'none'}
+                        />
+
+                        <View style={{ alignItems: 'center', marginTop: -5 }}>
+                            <Button
+                                title="send email"
+                                titleStyle={{ padding: 10, marginTop: -3, fontFamily: 'NotoSansBold', color: '#fc8181', fontSize: 14 }}
+                                buttonStyle={{ width: 300, height: 32, borderRadius: 10, backgroundColor: 'white' }}
+                                onPress={() => null}
+                            />
+                        </View>
+                    </View>
+                </KeyboardAvoidingView>
+
+                <LottieView
+                    ref={anim => { animation = anim; }}
+                    style={{
+                        width: 0.4 * Dimensions.get('window').width,
+                        height: 0.4 * Dimensions.get('window').width,
+                        backgroundColor: 'transparent',
+                        alignSelf: 'center',
+                        marginBottom: '60%'
+                    }}
+                    source={require('../assets/anim/8282-search-icon.json')}
+                />
+
+            </View>
         </LinearGradient>
     );
 }
@@ -74,20 +83,48 @@ const Recover = ({ navigation }) => {
 const styles = StyleSheet.create({
     inputBox: {
         backgroundColor: 'white',
-        opacity: 0.75,
         borderRadius: 10,
-        borderBottomColor: 'transparent'
+        shadowColor: 'black',
+        borderWidth: 1.5,
+        borderColor: 'white',
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.2,
+        shadowRadius: 1.4,
+        elevation: 5
+    },
+    inputBoxError: {
+        backgroundColor: 'white',
+        borderRadius: 10,
+        shadowColor: 'black',
+        borderWidth: 1.5,
+        borderColor: '#FC4E29',
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.2,
+        shadowRadius: 1.4,
+        elevation: 5
     },
     input: {
         fontFamily: 'NotoSansBold',
-        fontSize: 16,
+        fontSize: 15,
         padding: 10
+    },
+    inputError: {
+        fontFamily: 'NotoSansBold',
+        fontSize: 15,
+        padding: 10,
+        color: '#FC4E29'
     },
     label: {
         color: 'white',
         marginBottom: 3,
         fontSize: 16,
-        fontFamily: 'NotoSansBold'
+        fontFamily: 'NotoSans'
     }
 });
 
