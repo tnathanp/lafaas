e7209f0c1131097e3b3a102f2a5dbd47361b2758
Tanathan Pansuwan <tanathanp@gmail.com>
2021-04-16 01:02:31 +0700

    Prevent back button on reported page for android user

--- page/Reported.js
+++ page/Reported.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View } from 'react-native';
 import { Button } from 'react-native-elements';
 import { Text } from '../component/Text';
 import { LinearGradient } from 'expo-linear-gradient';
 
 const Reported = ({ navigation }) => {
+    let performBack = false;
+
+    useEffect(() => {
+        navigation.addListener('beforeRemove', e => {
+            if (!performBack) {
+                e.preventDefault();
+            } else {
+                navigation.dispatch(e.data.action);
+            }
+        });
+    }, [navigation]);
 
     return (
         <LinearGradient colors={['#fc8181', '#f6a085']} locations={[0.7, 1]} style={{ flex: 1 }}>
@@ -32,7 +43,10 @@ const Reported = ({ navigation }) => {
                         title="Back to item list"
                         titleStyle={{ padding: 10, marginTop: -3, fontFamily: 'NotoSansBold', color: '#fc8181', fontSize: 14 }}
                         buttonStyle={{ width: 300, height: 32, borderRadius: 10, backgroundColor: 'white' }}
-                        onPress={() => navigation.navigate('List')}
+                        onPress={() => {
+                            performBack = true;
+                            navigation.navigate('List');
+                        }}
                     />
                 </View>
             </View>
