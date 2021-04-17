103fb677a40bc0fa72ea1ea58f33c973d8778c02
Tanathan Pansuwan <tanathanp@gmail.com>
2021-04-17 19:29:47 +0700

    Add support for message box in QR page

--- page/QRCode.js
+++ page/QRCode.js
@@ -1,12 +1,44 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, Dimensions } from 'react-native';
+import { View, StyleSheet, Dimensions, Modal } from 'react-native';
+import { Button } from 'react-native-elements';
 import { Text } from '../component/Text';
 import { default as QRGenerator } from 'react-native-qrcode-svg';
 
-const QRCode = ({ navigation }) => {
+const QRCode = ({ route, navigation }) => {
+    const [error, showError] = useState(false);
+    const [msg, setMsg] = useState('');
+
+    useEffect(() => {
+        if (route.params?.msg) setMsg(route.params.msg);
+    }, [route.params])
+
+    useEffect(() => {
+        if (msg !== '') showError(true);
+    }, [msg])
 
     return (
         <View style={styles.container}>
+
+            <Modal
+                visible={error}
+                transparent={true}
+                animationType='fade'
+            >
+                <View style={{ backgroundColor: 'rgba(0,0,0,0.3)', width: '100%', height: '100%', alignItems: 'center', justifyContent: 'center' }}>
+                    <View style={{ backgroundColor: 'white', width: '90%', height: '50%', borderRadius: 20, alignItems: 'center' }}>
+
+                        <Text style={{ color: '#fc8181', fontWeight: 'bold', fontSize: 28, marginVertical: 20 }}>{msg}</Text>
+
+                        <Button
+                            title="Close"
+                            titleStyle={{ fontFamily: 'NotoSansBold', padding: '35%' }}
+                            buttonStyle={{ marginBottom: '5%', backgroundColor: '#fc8181', borderRadius: 10 }}
+                            onPress={() => showError(false)}
+                        />
+
+                    </View>
+                </View>
+            </Modal>
 
             {/*https://www.devglan.com/online-tools/aes-encryption-decryption*/}
             <QRGenerator size={0.6 * Dimensions.get('window').width} value='aeJshhikeV0l6BCOZH3qj1BJzElD3OQngAIMUqlrLOM=' />
