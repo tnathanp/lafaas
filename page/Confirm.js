af115221fa75a60e27b3ea98a6a885847575feaf
tnathanp <tanathanp@gmail.com>
2021-05-01 23:55:19 +0700

    Cancel with backend

--- page/Confirm.js
+++ page/Confirm.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, Dimensions } from 'react-native';
 import { Button } from 'react-native-elements';
 import { Text } from '../component/Text';
 import { LinearGradient } from 'expo-linear-gradient';
 
-const Confirm = ({ navigation }) => {
+const Confirm = ({ route, navigation }) => {
+
+    const [qrid, setQRid] = useState("");
+
+    useEffect(() => {
+        if (route.params?.qrid) setQRid(route.params.qrid);
+    }, [route.params])
+
+    function cancel() {
+        fetch('https://lafaas-n4hzx.ondigitalocean.app/cancelclaim' + qrid).then(res => res.text()).then(data => {
+            //Done
+        })
+    };
 
     return (
         <View style={styles.container}>
 
-            <Text style={{ color: '#fc8181', fontWeight: 'bold', fontSize: 48, marginTop: 20 }}>Thank You!</Text>
+            <Text style={{ color: '#fc8181', fontWeight: 'bold', fontSize: 48, marginTop: 20 }}>หน้าcancel</Text>
             <Text style={{ color: '#777777', fontSize: 16 }}>Let's hope the owner finds their item soon</Text>
 
             <Text style={{ color: '#fc8181', fontWeight: 'bold', fontSize: 18, textAlign: 'center', marginBottom: 20, marginTop: Dimensions.get('window').height * 0.35 }}>
@@ -18,7 +30,7 @@ const Confirm = ({ navigation }) => {
 
             <Button
                 title='Cancel'
-                onPress={() => null}
+                onPress={() => cancel()}
                 titleStyle={{ padding: 50, fontSize: 14, fontFamily: 'NotoSansBold', marginTop: -5, marginBottom: -3 }}
                 buttonStyle={{ borderRadius: 10, marginRight: 10 }}
                 ViewComponent={LinearGradient}
